d6f4d5c7fcdadcc0226fe0a91fd3400b969a6ba3
Vu Anh Khoi <135935358+Ekanara@users.noreply.github.com>
2025-06-17 17:09:53 +0700

    Add files via upload

---
+++ train_MRPC.py
@@ -0,0 +1,72 @@
+from datasets import load_dataset,  DatasetDict
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, DataCollatorWithPadding, AutoTokenizer
+import evaluate
+import numpy as np
+
+
+if __name__ =='__main__':
+
+    dataset = load_dataset("tmnam20/ViGLUE", subset='mrpc')
+    dataset.pop("test",None)  # Drop the test set
+    train_dataset = dataset["train"]
+    val_dataset = dataset["validation"]
+    model_name = "./checkpoint-116000"  
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    #print(train_dataset[2])
+    label_map = {"not_equivalent": 0, "equivalent": 1}  # Convert to numerical labels
+
+    def format_prompt(sentence1, sentence2, label = None):
+        prompt = f"<Sentence1>: {sentence1} </Sentence1>\n <Sentence2>: {sentence2} </Sentence2>"  # Convert sentence to lowercase
+        return prompt
+    #formatted_text = format_prompt("Ai đã lật ngược phán quyết của Taft Vale?", "Một trong những hành động đầu tiên của Chính phủ Tự do mới là đảo ngược phán quyết của Taff Vale.", 'entailment')
+    #print(formatted_text)
+
+    def preprocess_function(examples):
+        texts = [format_prompt(s1, s2) for s1, s2 in zip(examples["sentence1"], examples["sentence2"])]
+        tokenized = tokenizer(texts, truncation=True)
+        return {
+            "input_ids": tokenized["input_ids"],
+            "label": [label_map[label] for label in examples["label"]]  # Overwrite "label"
+        }
+
+    dataset = dataset.map(preprocess_function, batched=True)
+    #encoded_dataset.save_to_disk("qnli_SLM_preprocessed")
+    accuracy = evaluate.load("accuracy")
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        predictions = np.argmax(predictions, axis=1)
+        return accuracy.compute(predictions=predictions, references=labels)
+
+    id2label = {0: "not_equivalent", 1: "equivalent"}
+    label2id = {"not_equivalent": 0, "equivalent": 1}
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2, trust_remote_code=True, label2id = label2id, id2label=id2label)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt")
+
+    training_args = TrainingArguments(
+        output_dir="Qwenv2.5_MRPC_results",
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        logging_strategy="epoch",
+        lr_scheduler_type="cosine",
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        gradient_accumulation_steps=2,
+        num_train_epochs=5,
+        learning_rate=2e-5,
+        weight_decay=0.01,
+        bf16=True,  
+        load_best_model_at_end=True,
+        push_to_hub=False,  
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["validation"],
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
